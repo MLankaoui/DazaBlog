a43efccaae2776fa159e4ccb2d6a6063a7a1f484
MLankaoui <marouanelankaoui66@gmail.com>
2024-11-30 22:32:42 +0100

    finish the hero section pretty much.

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Header from "./components/Header.jsx";
+import Hero from "./components/Hero.jsx";
+import { scroll } from "motion"
 
 function App() {
 
   return (
     <>
-      <Header />
+        <Header />
+        <Hero />
     </>
   )
 }
