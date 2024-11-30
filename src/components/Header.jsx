fd075088a23d0aee854dbafa335209894e4defb8
MLankaoui <marouanelankaoui66@gmail.com>
2024-11-30 21:43:15 +0100

    finish pretty much the whole header.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,11 @@
 const Header = () => {
     return <>
-        <div className="header-container">
-            Header
-        </div>
+        <ul className="flex justify-around flex-wrap p-7 bg-dark bg-slate-800 text-gray-400 font-bold">
+            <li>Home</li>
+            <li>About me</li>
+            <li>Projects</li>
+            <li>Contact</li>
+        </ul>
     </>
 }
 
